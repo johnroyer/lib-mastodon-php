5da42664b0cb48c6894134d16255766fffa34ca7
Zero <john.royer@gmail.com>
2025-09-17 13:55:28 +0800

    add unit test prototype

---
+++ tests/BaseTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+final class BaseTest extends \PHPUnit\Framework\TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
