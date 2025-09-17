fe8ab0c36af660f8eb7932170b39df2e6eb86de2
Zero <john.royer@gmail.com>
2025-09-17 13:32:55 +0800

    add API client prototype

---
+++ src/Mastodon.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Zeroplex\LibMastodonPhp;
+
+class Mastodon
+{
+    private static self $instance;
+
+    private function __construct(
+        string $host,
+        int $port = 443,
+        array $header = null
+    )
+    {
+        //
+    }
+
+    public static function getInstance(
+        string $host,
+        int $port = 443,
+        array $header = null
+    )
+    {
+        if (!empty(self::$instance) {
+            return self::$instance;
+        }
+
+        self::$instance = new self(
+            $host,
+            $port,
+            $header
+        );
+
+        return self::$instance;
+    }
+}
